7d4f6a3be4f78bf2efd7d04447297c0b384b26f7
Abdullah ilham <91928537+Kenzoham@users.noreply.github.com>
2022-03-07 19:58:06 +0700

    Add files via upload

---
+++ praktik1-abdullahilham/variable_user.php
@@ -0,0 +1,17 @@
+<?php
+// data
+$nale= "Abdullah Ilham";
+$nama= "Ilham";
+$umur= 19;
+$berat= 45;
+echo "Nama : $nale";
+echo "<br>Nama Panggilan : $nama";
+echo "<br>Umur : $umur Tahun";
+echo "<br>Berat : $berat kg";
+
+// print
+echo "<br>Hello $nale apa kabar? ";
+echo "<br>Hai nama saya $nale, kalian bisa panggil saya $nama, umur saya $umur tahun, berat badan saya $berat kg. Salam kenal semuanya.";
+echo "<hr>";
+
+?>
